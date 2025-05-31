bed4e6ec75312f5862540ca540c90931ddeaf1b3
SergioAlarcon09 <sergio.alarcon01@uceva.edu.co>
2025-05-31 11:45:21 -0500

    Codelab IOT Receiver

--- src/secrets.cpp
+++ src/secrets.cpp
@@ -46,8 +46,8 @@
 
 // Variables de configuración de la red WiFi
 // Cambia estos valores por los de su red WiFi
-#define SSID "VIRUS2"                     ///< Cambia por el nombre de tu red WiFi
-#define PASSWORD "a1b2c3d4"                       ///< Cambia por la contraseña de tu red WiFi
+#define SSID "HERMANOS ALARCON"                     ///< Cambia por el nombre de tu red WiFi
+#define PASSWORD "1034396905*"                       ///< Cambia por la contraseña de tu red WiFi
 
 // Cambia al certificado del broker: Certificado raíz de Let's Encrypt (ISRG Root X1) en formato PEM
 const char* root_ca = \
